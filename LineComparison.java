ef57f79ffd632d9666f9c7ece0285b2cb78d37aa
chutki12 <priyankachaudhary2068>
2022-07-28 00:59:38 +0530

    ADDED : This Code For To Calculate Length Between Two Point

--- LineComparison.java
+++ LineComparison.java
@@ -1,11 +1,34 @@
 package com.bridgelabz.lineComparison;
 
-public class LineComparison {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+
+public class LineComparison {
+      int x1,x2,y1,y2;
+	  public static void calculateLength () {
 		
 		System.out.println("Welcome To Line Comparison Computation Program ");
+		Scanner scanner = new Scanner(System.in);
 
+	      System.out.println("Enter the Co-ordinates of x1 and y1 : ");
+	      int x1 =  scanner.nextInt();
+	      int y1 =  scanner.nextInt();
+	      System.out.println("Enter the Co-ordinates of x2 and y2 : ");
+	      int x2 =  scanner.nextInt();
+	      int y2 =  scanner.nextInt();
+	      
+	      double length = Math.sqrt((x2 - x1)^2  +  (y2 - y1)^2);
+	      System.out.println("Length of the given Line is : " + length + " units");
+	       
+	      scanner.close();
+	    } 
+	   
+	  public static void main(String[] args) {
+		  
+		  calculateLength ();
+		  
+	  }
+	  
 	}
 
-}
+
